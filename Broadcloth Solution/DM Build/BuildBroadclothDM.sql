b3780517eb7afc804f9e9b12e359c7d165e182bb
Marion <Marion.bania@gmail.com>
2018-10-17 16:57:12 -0600

    Updated SSIS

--- Broadcloth Solution/DM Build/BuildBroadclothDM.sql
+++ Broadcloth Solution/DM Build/BuildBroadclothDM.sql
@@ -56,13 +56,37 @@ CREATE TABLE DimDate
 	(
 	Date_SK				INT PRIMARY KEY, 
 	Date				DATE,
-	Year				INT,-- Year value of Date stored in Row
+	FullDate			NCHAR(10),-- Date in MM-dd-yyyy format
+	DayOfMonth			INT, -- Field will hold day number of Month
+	DayName				NVARCHAR(9), -- Contains name of the day, Sunday, Monday 
+	DayOfWeek			INT,-- First Day Sunday=1 and Saturday=7
+	DayOfWeekInMonth	INT, -- 1st Monday or 2nd Monday in Month
+	DayOfWeekInYear		INT,
+	DayOfQuarter		INT,
+	DayOfYear			INT,
+	WeekOfMonth			INT,-- Week Number of Month 
+	WeekOfQuarter		INT, -- Week Number of the Quarter
+	WeekOfYear			INT,-- Week Number of the Year
 	Month				INT, -- Number of the Month 1 to 12{}
+	MonthName			NVARCHAR(9),-- January, February etc
+	MonthOfQuarter		INT,-- Month Number belongs to Quarter
 	Quarter				NCHAR(2),
+	QuarterName			NVARCHAR(9),-- First,Second..
+	Year				INT,-- Year value of Date stored in Row
+	YearName			CHAR(7), -- CY 2017,CY 2018
+	MonthYear			CHAR(10), -- Jan-2018,Feb-2018
+	MMYYYY				INT,
+	FirstDayOfMonth		DATE,
+	LastDayOfMonth		DATE,
+	FirstDayOfQuarter	DATE,
+	LastDayOfQuarter	DATE,
+	FirstDayOfYear		DATE,
+	LastDayOfYear		DATE,
+	IsHoliday			BIT,-- Flag 1=National Holiday, 0-No National Holiday
+	IsWeekday			BIT,-- 0=Week End ,1=Week Day
 	Holiday				NVARCHAR(50),--Name of Holiday in US
 	Season				NVARCHAR(10)--Name of Season
 	);
---
 
 
 --Create tables
@@ -73,7 +97,7 @@ CREATE TABLE DimFactory
 	Factory_AK			INT NOT NULL,
 	Nation				NVARCHAR(50) NOT NULL,
 	City				NVARCHAR(50) NOT NULL,
-	OverallRating		NUMERIC(18,0) NOT NULL,
+	OverallRating		NVARCHAR(4) NOT NULL,
 	MaxWorkers			INT NOT NULL,
 
 	);
